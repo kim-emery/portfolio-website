3998d79bfc4d03ad6d8d523f8ed05eb74996c821
Kim <kimberley.emery@gmail.com>
2019-07-02 22:02:39 +1200

    changed structure to common standard, added vue and js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* v1.0.0 
+Main JavaScript file with the addition of Vue.js 
+
+*/
+
+//binding element attributes syntax
+var app = new Vue({
+    el: '#app', //element to apply to
+    data: {
+        message: 'Hello Vue'
+    }
+})
+
+window.addEventListener('load', function(){
+    
+})
